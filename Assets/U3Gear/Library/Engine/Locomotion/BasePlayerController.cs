28c7981b11a08e0a689f80e87901605281d84721
Clemente Nogueira <git.3gear@gmail.com>
2022-03-20 18:42:53 +0000

    Changes have been made to our Player's movement.

--- Assets/U3Gear/Library/Engine/Locomotion/BasePlayerController.cs
+++ Assets/U3Gear/Library/Engine/Locomotion/BasePlayerController.cs
@@ -10,6 +10,8 @@ namespace U3Gear.Library.Engine.Locomotion
         [SerializeField]
         private float speed; // speed of the player
 
+        private Vector3 _movement; // movement position of the player
+        
         /// <summary>
         /// Fixed update the player position and velocity
         /// </summary>
@@ -17,9 +19,9 @@ namespace U3Gear.Library.Engine.Locomotion
         {
             var horizontalInput = Input.GetAxis("Horizontal"); // horizontal position of the player
             var verticalInput = Input.GetAxis("Vertical"); // vertical position of the player
-            var movement = new Vector3(horizontalInput, 0, verticalInput); // movement direction
-            movement.Normalize(); // normalize movement direction
-            transform.Translate(movement * speed * Time.fixedDeltaTime); // transform position
+            _movement = new Vector3(horizontalInput, 0, verticalInput); // movement direction
+            _movement.Normalize(); // normalize movement direction
+            transform.Translate(_movement * speed * Time.fixedDeltaTime); // transform position
         }
     }
 }
