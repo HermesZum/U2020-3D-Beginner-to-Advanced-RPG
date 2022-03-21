057d49be41bd94409e00c69eaca286e7282a9e70
Clemente Nogueira <git.3gear@gmail.com>
2022-03-21 14:40:31 +0000

    Capsule Collider and Rigidbody components have been added to the Player.

--- Assets/U3Gear/Library/Engine/Locomotion/BasePlayerController.cs
+++ Assets/U3Gear/Library/Engine/Locomotion/BasePlayerController.cs
@@ -12,6 +12,17 @@ namespace U3Gear.Library.Engine.Locomotion
 
         private Vector3 _movement; // movement position of the player
         
+        private Rigidbody _rigidbody; // rigid body that is attached to the player
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled
+        /// just before any of the Update methods are called the first time.
+        /// </summary>
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>(); // initiates the reference to the rigid body
+        }
+        
         /// <summary>
         /// Fixed update the player position and velocity
         /// </summary>
@@ -21,7 +32,7 @@ namespace U3Gear.Library.Engine.Locomotion
             var verticalInput = Input.GetAxis("Vertical"); // vertical position of the player
             _movement = new Vector3(horizontalInput, 0, verticalInput); // movement direction
             _movement.Normalize(); // normalize movement direction
-            transform.Translate(_movement * speed * Time.fixedDeltaTime); // transform position
+            _rigidbody.MovePosition(_rigidbody.position + _movement * speed * Time.fixedDeltaTime); // rigidbody movement position
         }
     }
 }
