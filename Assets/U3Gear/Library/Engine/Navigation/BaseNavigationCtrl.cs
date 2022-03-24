340163136a11c3cb26e6a724025a08bf1b95918a
Clemente Nogueira <git.3gear@gmail.com>
2022-03-24 17:15:01 +0000

    Improved player movement and camera tracking.

--- Assets/U3Gear/Library/Engine/Navigation/BaseNavigationCtrl.cs
+++ Assets/U3Gear/Library/Engine/Navigation/BaseNavigationCtrl.cs
@@ -8,17 +8,16 @@ namespace U3Gear.Library.Engine.Navigation
     public abstract class BaseNavigationCtrl : MonoBehaviour
     {
         private float Speed { get; set; } = 3; // speed of the player
-        private float RotationSpeed { get; set; } = 9; // rotation speed of the player
         private Rigidbody RigidBody { get; set; } // rigidbody that is attached to the player
-        private Vector3 Movement { get; set; } // movement position of the player
-        private Quaternion Rotation { get; set; } // rotation of the player
-        
+        private Camera CameraMain  { get; set; } // main camera 
+
         /// <summary>
         /// Awake is called when the script instance is being loaded.
         /// </summary>
         protected virtual void Awake()
         {
             RigidBody = GetComponent<Rigidbody>(); // initiates the reference to the rigid body
+            CameraMain = Camera.main;
         }
         
         /// <summary>
@@ -26,15 +25,20 @@ namespace U3Gear.Library.Engine.Navigation
         /// </summary>
         protected virtual void FixedUpdate()
         {
-            var horizontalInput = Input.GetAxis("Horizontal"); // horizontal position of the player
-            var verticalInput = Input.GetAxis("Vertical"); // vertical position of the player
-            Movement = new Vector3(horizontalInput, 0, verticalInput); // movement direction
-            Movement.Normalize(); // normalize movement direction
-            var interval = Time.fixedDeltaTime;
-            var direction = Vector3.RotateTowards(transform.forward, Movement, interval * RotationSpeed, 0); // locate player rotation direction
-            Rotation = Quaternion.LookRotation(direction); // player rotation with the specified forward and upwards directions.
-            RigidBody.MovePosition(RigidBody.position + Movement * Speed * interval); // rigidbody movement position
-            RigidBody.MoveRotation(Rotation); // rigidbody rotation with the specified direction
+            var inputDirection = Vector3.zero; // initial input direction of the player movement
+            inputDirection.x = Input.GetAxis("Horizontal"); // input direction in the X axis (Horizontal)
+            inputDirection.z = Input.GetAxis("Vertical"); // input direction in the Z axis (Vertical)
+            if(inputDirection == Vector3.zero) return; // if not using input, then return
+            var cameraDirection = CameraMain.transform.rotation * inputDirection; // position camera direction in relation to main camera rotation multiplied by movement input
+            var targetDirection = new Vector3(cameraDirection.x, 0, cameraDirection.z); // position of target direction in relation to the camera direction
+            // if player is moving in forward direction
+            if (inputDirection.z >= 0)
+            {
+                // change player rotation
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), 0.1f);
+            }
+            var interval = Time.fixedDeltaTime; // interval of time
+            RigidBody.MovePosition(RigidBody.position + targetDirection.normalized * (Speed * interval)); // rigidbody movement position
         }
     }
 }
