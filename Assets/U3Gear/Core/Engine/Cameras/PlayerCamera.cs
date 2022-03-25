b1287470203d7290c61dbe4d0d224fc47f3c0bbd
Clemente Nogueira <git.3gear@gmail.com>
2022-03-25 21:57:03 +0000

    The Cinemachine package was installed. The logic for the camera movement was created.

--- Assets/U3Gear/Core/Engine/Cameras/PlayerCamera.cs
+++ Assets/U3Gear/Core/Engine/Cameras/PlayerCamera.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using U3Gear.Library.Engine.Cameras;
 using UnityEngine;
 
@@ -5,32 +6,36 @@ namespace U3Gear.Core.Engine.Cameras
 {
     internal sealed class PlayerCamera : BaseCamerasCtrl
     {
-        /// <summary>
-        /// Position the camera relative two points
-        /// </summary>
-        private Vector3 Position
-        {
-            get => transform.position;
-            set => transform.position = value;
-        }
-        
-        /// <summary>
-        ///  Camera rotation direction relative to the player
-        /// </summary>
-        private Vector3 Angles
-        {
-            get => transform.eulerAngles;
-            set => transform.eulerAngles = value;
-        }
+        private static GameObject CmFreeLookCamera { get; set; } // reference to free look camera GameObject
+        private CinemachineFreeLook FreeLookCamera { get; set; } // reference to Cinemachine free look camera
+
 
         /// <summary>
         ///     Awake is called when the script instance is being loaded.
         /// </summary>
         protected override void Awake()
         {
-            if (Target == null)
-                Target = GameObject.Find("Player");
+            CmFreeLookCamera = GameObject.Find("/Cameras/CM FreeLookCamera"); // return to cache the CM FreeLookCamera GameObject
+            FreeLookCamera = CmFreeLookCamera.GetComponent<CinemachineFreeLook>(); // return to cache the CinemachineFreeLook component
         }
+        
+        /// <summary>
+        ///     Update is called every frame, if the MonoBehaviour is enabled.
+        ///     Update is the most commonly used function to implement any kind of game behavior.
+        /// </summary>
+        protected override void Update()
+        {
+            if (Input.GetMouseButtonDown(1)) // if press and hold the right mouse button
+            {
+                FreeLookCamera.m_XAxis.m_MaxSpeed = 400; // set the camera max speed in the X axis
+                FreeLookCamera.m_YAxis.m_MaxSpeed = 10; // set the camera max speed in the Y axis
+            }
+            else if (Input.GetMouseButtonUp(1)) // if releases the right mouse button
+            {
+                FreeLookCamera.m_XAxis.m_MaxSpeed = 0; // set the camera max speed in the X axis
+                FreeLookCamera.m_YAxis.m_MaxSpeed = 0; // set the camera max speed in the Y axis               
+            }
+        }       
 
         /// <summary>
         ///     LateUpdate is called after all Update functions have been called.
@@ -40,18 +45,6 @@ namespace U3Gear.Core.Engine.Cameras
         /// </summary>
         protected override void LateUpdate()
         {
-            if(!Target) return; // if not have target then return
-            
-            var angles = Angles.y; // acquire current rotation angle y for the camera
-            var targetAngles = TargetAngles.y; // acquire current target rotation angle y for the target
-            // Same as Lerp but makes sure the values interpolate correctly when they wrap around 360 degrees.
-            // The parameter t is clamped to the range [0, 1]. Variables a and b are assumed to be in degrees.
-            angles = Mathf.LerpAngle(angles, targetAngles, 0.5f); // update current rotation angles for the camera relative to the target
-            Position = new Vector3(TargetPosition.x, TargetPosition.y + 5f, TargetPosition.z); // update current position for the camera
-            var rotation = Quaternion.Euler(0, angles, 0); // acquire current rotation around Y for the camera
-            var rotatedPosition = rotation * Vector3.forward; // acquire the camera rotation to the forward vector
-            Position -= rotatedPosition * 10; // update current position for the camera but with the camera behind it
-            transform.LookAt(Target.transform); // camera towards the target 
         }
     }
 }
--- Assets/U3Gear/Library/Engine/Cameras/BaseCamerasCtrl.cs
+++ Assets/U3Gear/Library/Engine/Cameras/BaseCamerasCtrl.cs
@@ -4,27 +4,29 @@ namespace U3Gear.Library.Engine.Cameras
 {
     public abstract class BaseCamerasCtrl : MonoBehaviour
     {
-        protected GameObject Target { get; set; } // The target object that will be used to control the camera
-        protected Vector3 TargetPosition => Target.transform.position; // The position of the camera's target
-        protected Vector3 TargetAngles => Target.transform.eulerAngles; // The rotation angles of the camera's target'
-        
         /// <summary>
-        /// Awake is called when the script instance is being loaded.
+        ///     Awake is called when the script instance is being loaded.
         /// </summary>
         protected virtual void Awake()
         {
-
         }
 
         /// <summary>
-        /// LateUpdate is called after all Update functions have been called.
-        /// This is useful to order script execution.
-        /// For example a follow camera should always be implemented in LateUpdate
-        /// because it tracks objects that might have moved inside Update.
+        ///     Update is called every frame, if the MonoBehaviour is enabled.
+        ///     Update is the most commonly used function to implement any kind of game behavior.
+        /// </summary>
+        protected virtual void Update()
+        {
+        }
+
+        /// <summary>
+        ///     LateUpdate is called after all Update functions have been called.
+        ///     This is useful to order script execution.
+        ///     For example a follow camera should always be implemented in LateUpdate
+        ///     because it tracks objects that might have moved inside Update.
         /// </summary>
         protected virtual void LateUpdate()
         {
-
         }
     }
 }
