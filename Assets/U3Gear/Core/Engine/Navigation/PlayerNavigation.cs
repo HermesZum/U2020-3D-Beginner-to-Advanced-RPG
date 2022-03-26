0e50b91f1cbc342e472237223ea820db8cec09bc
Clemente Nogueira <git.3gear@gmail.com>
2022-03-26 19:35:31 +0000

    The logic for the rotation of the camera and the Player was created.

--- Assets/U3Gear/Core/Engine/Navigation/PlayerNavigation.cs
+++ Assets/U3Gear/Core/Engine/Navigation/PlayerNavigation.cs
@@ -5,6 +5,37 @@ namespace U3Gear.Core.Engine.Navigation
 {
     internal sealed class PlayerNavigation : BaseNavigationCtrl
     {
+        private float Speed { get; set; } = 3; // speed of the player
+        private float RotationSpeed { get; set; } = 9; // rotation speed of the player
+        private Rigidbody RigidBody { get; set; } // rigidbody that is attached to the player
+        private static Camera MainCamera { get; set; } // reference to the Main Camera
 
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        protected override void Awake()
+        {
+            RigidBody = GetComponent<Rigidbody>(); // initiates the reference to the rigid body
+            MainCamera = Camera.main;  // initiates the reference to the Main Camera
+        }
+        
+        /// <summary>
+        /// Fixed update the player position and velocity
+        /// </summary>
+        protected override void FixedUpdate()
+        {
+            var horizontalInput = Input.GetAxis("Horizontal"); // horizontal input of the player
+            var verticalInput = Input.GetAxis("Vertical"); // vertical input of the player
+            var movement = Vector3.zero; // initial movement position
+            movement.Set(horizontalInput, 0, verticalInput); // movement input
+            var cameraRotation = MainCamera.transform.rotation; // reference to the main camera rotation
+            var targetDirection = cameraRotation * movement; // reference to the target direction
+            targetDirection.y = 0; // target direction in the Y axis must be 0
+            targetDirection.Normalize(); // target direction must have a magnitude of 1
+            var interval = Time.fixedDeltaTime; // interval of time
+            RigidBody.MovePosition(RigidBody.position + targetDirection * (Speed * interval)); // rigidbody movement position
+            var rotation = Quaternion.Euler(0, cameraRotation.eulerAngles.y, 0); // camera rotation
+            RigidBody.MoveRotation(rotation); // movement rotation
+        }
     }
 }
--- Assets/U3Gear/Library/Engine/Navigation/BaseNavigationCtrl.cs
+++ Assets/U3Gear/Library/Engine/Navigation/BaseNavigationCtrl.cs
@@ -7,16 +7,11 @@ namespace U3Gear.Library.Engine.Navigation
     /// </summary>
     public abstract class BaseNavigationCtrl : MonoBehaviour
     {
-        private float Speed { get; set; } = 3; // speed of the player
-        private float RotationSpeed { get; set; } = 9; // rotation speed of the player
-        private Rigidbody RigidBody { get; set; } // rigidbody that is attached to the player
-
         /// <summary>
         /// Awake is called when the script instance is being loaded.
         /// </summary>
-        protected virtual void Start()
+        protected virtual void Awake()
         {
-            RigidBody = GetComponent<Rigidbody>(); // initiates the reference to the rigid body
         }
         
         /// <summary>
@@ -24,16 +19,6 @@ namespace U3Gear.Library.Engine.Navigation
         /// </summary>
         protected virtual void FixedUpdate()
         {
-            var horizontalInput = Input.GetAxis("Horizontal"); // horizontal position of the player
-            var verticalInput = Input.GetAxis("Vertical"); // vertical position of the player
-            var movement = Vector3.zero;
-            movement.Set(horizontalInput, 0, verticalInput); // movement input
-            movement.Normalize(); // update and normalize the player position
-            var interval = Time.fixedDeltaTime; // interval of time
-            var direction = Vector3.RotateTowards(transform.forward, movement, RotationSpeed * interval, 0); // direction of movement
-            var rotation = Quaternion.LookRotation(direction);
-            RigidBody.MovePosition(RigidBody.position + movement * (Speed * interval)); // rigidbody movement position
-            RigidBody.MoveRotation(rotation);
         }
     }
 }
